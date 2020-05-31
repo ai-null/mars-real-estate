02b9a4160c486978cb8c77697b9490c86701ff2a
MAinulYaqin <ainul.y9b@gmail.com>
2020-05-31 22:25:22 +0700

    add rent mark on each list, to show which to rent and buy

--- app/src/main/java/com/gabutproject/mars_rent/network/MarsProperty.kt
+++ app/src/main/java/com/gabutproject/mars_rent/network/MarsProperty.kt
@@ -4,8 +4,9 @@ import com.squareup.moshi.Json
 
 data class MarsProperty(
     val id: String,
-    @Json(name = "img_src")
-    val imgSrcUrl: String,
+    @Json(name = "img_src") val imgSrcUrl: String,
     val type: String,
     val price: Double
-)
+) {
+    val isRental get() = type == "rent"
+}
